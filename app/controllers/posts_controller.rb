f04251eccad69364d8cee1678e5fa1a0a49a040c
oppen <arieloppenheimer@outlook.cl>
2022-04-22 15:46:11 -0400

    4 Respaldo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
+    @posts=Post.all.order("created_at DESC")
   end
 
   def create
+    @post=Post.create(title: params[:titulo], image_url: params[:url_img],content: params[:contenido])
   end
 end
